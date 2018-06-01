5587c275d2483f636cae557b90332d7120c2c031
Alexey Vasilev <escantor@gmail.com>
2018-06-01 16:17:49 +0300

    remove easy thumbnails from installation

--- hasker/hasker/settings.py
+++ hasker/hasker/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'easy_thumbnails',
     'django.contrib.postgres',
     'questions',
 ]
