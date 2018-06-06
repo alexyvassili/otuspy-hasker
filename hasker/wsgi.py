2ad51453435317392efc157d45c3af80781e3e6a
Alexey Vasilev <escantor@gmail.com>
2018-06-06 15:07:43 +0300

    add prod to wsgi.py

--- hasker/wsgi.py
+++ hasker/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 # from django.core.wsgi import get_wsgi_application
 
+os.environ.setdefault("DJANGO_CONFIGURATION", "Prod")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hasker.settings")
 
 from configurations.wsgi import get_wsgi_application
