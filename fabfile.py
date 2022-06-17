b094401fd863eab8006c6d4a92d2c08efb815f95
Alexey Vasilev <escantor@yandex-team.ru>
2022-06-18 03:10:49 +0600

    Update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,10 @@
 """
-    This is fabfile for deploying hasker on debian stretch on custom python 3.6.5
-    You can manually install python3.6 to /usr/local/bin e.g. from Anaconda distribution
-    or this script will install and compile python3.6 and wsgi plugin automatically
+    This is fabfile for deploying hasker on Debian 11 and Python 3.9.2
 """
 import os
-import sys
 
 from fabric.state import env
-from fabric.api import cd, run, sudo, settings
+from fabric.api import run, sudo
 from fabric.contrib.files import exists, upload_template
 
 from hasker.secrets import DB_PASSWORD, DB_USER, SUPERUSER, SUPERUSER_MAIL, SUPERUSER_PASS
@@ -26,10 +23,6 @@ def bootstrap():
     run('uname -a')
     input('prepare package system')
     prepare_package_system()
-    input('prepare_interpreter')
-    prepare_interpreter()
-    input('prepare_uwsgi')
-    prepare_uwsgi()
     input('install_system_libs')
     install_system_libs()
     input('create_folders')
@@ -40,6 +33,8 @@ def bootstrap():
     set_secrets()
     input('create_virtualenv')
     create_virtualenv()
+    input('prepare_uwsgi')
+    prepare_uwsgi()
     input('install_venv_libs')
     install_venv_libs()
     input('configure_postgresql')
@@ -75,96 +70,25 @@ def set_env():
     env.REMOTE_VENV_PATH = os.path.join(env.VENV_PATH,
                                         env.PROJECT_NAME)
     env.GIT_REPO_PATH = "https://github.com/alexyvassili/otuspy-hasker.git"
-    if exists('/usr/bin/python3.6'):
-        env.BASE_REMOTE_PYTHON_PATH = '/usr/bin/python3.6'
+    if exists('/usr/bin/python3.9'):
+        env.BASE_REMOTE_PYTHON_PATH = '/usr/bin/python3.9'
+    elif exists('/usr/local/bin/python3.9'):
+        env.BASE_REMOTE_PYTHON_PATH = '/usr/local/bin/python3.9'
     else:
-        env.BASE_REMOTE_PYTHON_PATH = '/usr/local/bin/python3.6'
-    env.VENV_REMOTE_PYTHON_PATH = os.path.join(env.REMOTE_VENV_PATH, 'bin', 'python3.6')
+        raise ValueError('You need install Python 3.9 or change Python version.')
+    env.VENV_REMOTE_PYTHON_PATH = os.path.join(env.REMOTE_VENV_PATH, 'bin', 'python3.9')
     env.DJANGO_CONFIGURATION = 'Prod'
 
 
 def prepare_package_system():
-    if not exists('/etc/apt/sources.list.old'):
-        sudo('mv /etc/apt/sources.list /etc/apt/sources.list.old')
-        upload_template('fabdeploy/sources.list', '/etc/apt/',use_sudo=True)
     sudo('apt-get update && apt-get upgrade')
     sudo('apt-get install -y aptitude')
     sudo('aptitude install -y mc vim git')
 
 
-def prepare_interpreter():
-    if not exists(env.BASE_REMOTE_PYTHON_PATH):
-        need_compile = input("""Python 3.6 interpreter not found.
-                             Do you want download precompiled from git repo? (y/n) """)
-        if need_compile.lower() == 'y':
-            sudo('apt-get install -y make build-essential libssl-dev zlib1g-dev')
-            sudo('apt-get install -y libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm')
-            sudo('apt-get install -y libncurses5-dev libncursesw5-dev xz-utils tk-dev')
-            if exists('/var/tmp/Python-3.6.5'):
-                run('rm -rf /var/tmp/Python-3.6.5')
-            run('git clone https://github.com/alexyvassili/python-3.6.5-precompiled.git /var/tmp/Python-3.6.5')
-            sudo('cd /var/tmp/Python-3.6.5; make altinstall')
-        elif need_compile.lower() == 'n':
-            print('OK, exiting')
-            sys.exit(1)
-        else:
-            prepare_interpreter()
-
-
-def compile_interpreter():
-    set_env()
-    if not exists(env.BASE_REMOTE_PYTHON_PATH):
-        need_compile = input("""Python 3.6 interpreter not found.
-                             Do you want download and compile this? (y/n) """)
-        if need_compile.lower() == 'y':
-            sudo('apt-get install -y make build-essential libssl-dev zlib1g-dev')
-            sudo('apt-get install -y libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm')
-            sudo('apt-get install -y libncurses5-dev libncursesw5-dev xz-utils tk-dev')
-            if exists('/tmp/Python-3.6.5.tgz'):
-                run('rm /tmp/Python-3.6.5.tgz')
-            run('wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz -O /tmp/Python-3.6.5.tgz')
-            run('tar xvf /tmp/Python-3.6.5.tgz -C /tmp/')
-            cd('/tmp/Python-3.6.5')
-            #  --enable-shared if you want use system uwsgi (but you don't want this!)
-            run('cd /tmp/Python-3.6.5; /tmp/Python-3.6.5/configure --enable-optimizations --with-ensurepip=install')
-            run('cd /tmp/Python-3.6.5; make -j8')
-            sudo('cd /tmp/Python-3.6.5; make altinstall')
-        elif need_compile.lower() == 'n':
-            print('OK, exiting')
-            sys.exit(1)
-        else:
-            compile_interpreter()
-
-
-def prepare_uwsgi():
-    """you can verify correct uwsgi and django installation by command
-        (after run django postinstall commands)
-        $ source /var/pyvenvs/hasker/bin/activate
-        $ python manage.py runserver
-        and if it's run ok, then
-        uwsgi --http :8080 --virtualenv /var/pyvenvs/hasker/ \
-             --chdir /var/www/hasker/ -w hasker.wsgi
-        if not, try:
-        uwsgi --plugins http,python36 --http :8080 --virtualenv /var/pyvenvs/hasker/ \
-             --chdir /var/www/hasker/ -w hasker.wsgi
-    """
-
-    # you can install system uwsgi, but it is not worked with python3.6
-    # you need compile python with --enable-shared parameter and
-    # compile uwsgi plugin:
-    # sudo('aptitude install python3 python-dev python3-dev uwsgi uwsgi-src uuid-dev libcap-dev libpcre3-dev')
-    # run('export PYTHON=python3.6')
-    # if not exists('/usr/lib/uwsgi/plugins/python36_plugin.so'):
-    #     run('uwsgi --build-plugin "/usr/src/uwsgi/plugins/python python36"')
-    #     sudo('mv python36_plugin.so /usr/lib/uwsgi/plugins/python36_plugin.so')
-    #     sudo('chmod 644 /usr/lib/uwsgi/plugins/python36_plugin.so')
-    # if python compiled with --enable-shared it can't found his own libraries, run:
-    # sudo('ldconfig')
-    sudo('/usr/local/bin/pip3.6 install uwsgi')
-
-
 def install_system_libs():
     sudo('aptitude install -y postgresql nginx git')
+    sudo('aptitude install -y python3-venv python3-pip python3-dev python3-wheel libpq-dev')
 
 
 def create_folders():
@@ -189,8 +113,26 @@ def set_secrets():
 def create_virtualenv():
     if not exists(env.VENV_REMOTE_PYTHON_PATH):
         run(f'{env.BASE_REMOTE_PYTHON_PATH} -m venv {env.REMOTE_VENV_PATH}')
-        pip = os.path.join(env.REMOTE_VENV_PATH, 'bin', 'pip3')
-        run(f'{pip} install --upgrade pip')
+        run(f'{env.VENV_REMOTE_PYTHON_PATH} -m pip install --upgrade pip')
+        run(f'{env.VENV_REMOTE_PYTHON_PATH} -m pip install wheel')
+
+
+def prepare_uwsgi():
+    """you can verify correct uwsgi and django installation by command
+        (after run django postinstall commands)
+        $ source /var/pyvenvs/hasker/bin/activate
+        $ python manage.py runserver
+        and if it's run ok, then
+        uwsgi --http :8080 --virtualenv /var/pyvenvs/hasker/ \
+             --chdir /var/www/hasker/ -w hasker.wsgi
+        if not, try:
+        uwsgi --plugins http,python36 --http :8080 --virtualenv /var/pyvenvs/hasker/ \
+             --chdir /var/www/hasker/ -w hasker.wsgi
+    """
+
+    sudo(f'{env.VENV_REMOTE_PYTHON_PATH} -m pip install uwsgi')
+    if not exists('/usr/bin/uwsgi'):
+        sudo(f'ln -s {os.path.join(env.REMOTE_VENV_PATH, "bin", "uwsgi")} /usr/bin/uwsgi')
 
 
 def install_venv_libs():
